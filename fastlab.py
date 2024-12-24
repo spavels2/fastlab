ef9c78903c24e289f8b92ca1f6cd34ea73f6cbeb
spavels2 <spavels2@mail.ru>
2024-12-25 00:37:10 +0800

    adding unit testing

--- fastlab.py
+++ fastlab.py
@@ -3,9 +3,12 @@ import uvicorn
 
 app = FastAPI()
 
+def sum_two_args(x,y):
+    return x + y
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
